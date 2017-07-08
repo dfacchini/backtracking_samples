08bdebde9d5f202a5da30a766c59143558b102eb
dfacchini <douglas.facchini@gmail.com>
2017-07-07 21:39:18 -0300

    Teste de sub-blocos.

--- backtracking.py
+++ backtracking.py
@@ -1,6 +1,11 @@
 # -*- coding: utf-8 -*-
-import random
-from itertools import groupby
+
+
+def busca_estado_vazio(mapa):
+    for x in range(0, len(mapa)):
+        for y in range(0, len(mapa)):
+            if mapa[x][y] == 0:
+                return (x, y)
 
 
 def valor_valido(mapa, x, y, valor):
@@ -11,24 +16,41 @@ def valor_valido(mapa, x, y, valor):
         return False
 
     for indice in range(0, len(mapa)):
-        if mapa[x][indice] == valor:
+        if mapa[x][indice] == valor or mapa[indice][y] == valor:
             return False
-        if mapa[indice][y] == valor:
-            return False
+
+    quandrante_x = x/3*3
+    quandrante_y = y/3*3
+
+    for indice_x in range(quandrante_x, quandrante_x+3):
+        for indice_y in range(quandrante_y, quandrante_x+3):
+            if mapa[quandrante_x][quandrante_y] == valor:
+                return False
 
     return True
 
 
 def backtracking(mapa):
-    for x in range(0, len(mapa)):
-        for y in range(0, len(mapa)):
-            if mapa[x][y] == 0:
-                pass
+
+    posicao = busca_estado_vazio(mapa)
+    if not posicao:
+        return True
+    for tester in range(1, 10):
+        if valor_valido(mapa, posicao[0], posicao[1], tester):
+            mapa[posicao[0]][posicao[1]] = tester
+            if backtracking(mapa):
+                return True
             else:
-                for tester in range(1, 10):
-                    if valor_valido(mapa, x, y, tester):
-                        mapa[x][y] = tester
-                        break
+                mapa[posicao[0]][posicao[1]] = 0
+
+
+def print_mapa(mapa):
+    for areas in mapa:
+        area = ''
+        for celula in areas:
+            area += '%s  ' % celula
+        print area + '\n'
+    print '\n'
 
 
 def main():
@@ -54,15 +76,13 @@ def main():
         print area + '\n'
     print '\n'
 
-
     print '\n Sub-Mapa\n'
-    for areas in sub_mapa:
-        area = ''
-        for celula in areas:
-            area += '%s  ' % celula
-        print area + '\n'
-    print '\n'
+    print_mapa(sub_mapa)
 
+    backtracking(sub_mapa)
+
+    print '\n Sub-Mapa Resolvido\n'
+    print_mapa(sub_mapa)
 
     from IPython import embed; embed()
 
